ce107ba97457504093ec762b72658880888cb501
Eric Park <epark03@uoguelph.ca>
2019-11-17 20:17:22 -0500

    Final commit: Includes hash functions in source folder and hash header file in submission format

--- a4.h
+++ a4.h
@@ -5,31 +5,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-struct record
-{
-    char *last_name;
-    char *first_name;
-    char *license_no;
-    char *license_type;
-    char *issue_date;
-};
-
-
-struct array
-{
-    struct record *arr;
-    int nelements;
-    struct record **hash;
-    int hash_size;
-};
+#include <math.h>
 
 int hash1(char *string, int hash_size);
 int hash2(char *string, int hash_size);
 int hash3(char *string, int hash_size);
-int char2int( unsigned char c );
-int str2int( char *s, int max );
-struct array *read_records();
-void build_hash( struct array *arrptr, int hash_size );
-void free_array_ptr( struct array *ptr );
-struct record *find( char *key, struct array *arrptr );
